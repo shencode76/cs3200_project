d72ef47e470aca45aa400a784916086f7a1d23e3
shencode76 <76721142+shencode76@users.noreply.github.com>
2021-04-18 15:05:41 +0800

    update search_song, search_artist, search_album

--- function&procedure.sql
+++ function&procedure.sql
@@ -22,34 +22,50 @@ _name like '% ? %';
 */
 
 -- only search for given album
+drop procedure search_album;
 DELIMITER $$
 CREATE PROCEDURE search_album(in given_album VARCHAR(30))
 BEGIN
-	if (search_album in (select album_name from Album))
-    then select * from Album where album_name = search_album ;
+	if (given_album in (select album_name from Album))
+    then select album_name, song_name from Album 
+	join Song on album_id = song_in_album
+	and album_name =  given_album;
     end if;
 END$$
 DELIMITER ;
+call search_album("Loud");
+
 
 -- only search for given song
+drop procedure search_song;
 DELIMITER $$
 CREATE PROCEDURE search_song(in given_song VARCHAR(30))
 BEGIN
 	if (given_song in (select song_name from Song))
-    then select * from Song where song_name = given_song ;
+    then select album_name, song_name, artist_name from Album 
+	join Song on album_id = song_in_album
+	join Artist on artist_id = singer_id
+	and song_name = given_song;
     end if;
 END$$
 DELIMITER ;
+call search_song("What's My Name");
 
 -- only search for given artist
 DELIMITER $$
 CREATE PROCEDURE search_artist(in given_artist VARCHAR(30))
 BEGIN
 	if (given_artist in (select artist_name from Artist))
-    then select * from Artist where artist_name = given_artist ;
+    then select artist_name, genre_name, album_name, song_name from Album 
+	join Song on album_id = song_in_album
+	join Artist on artist_id = singer_id
+	join Genre on artist_genre = genre_id
+	and artist_name = given_artist;
     end if;
 END$$
 DELIMITER ;
+call search_artist("Rihanna");
+call search_artist("Suede");
 
 
 -- generate all the artist, song, album that have given searcing condition 
